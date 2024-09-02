a58a4c1bcc69499f465984743f285b08074d9efb
Morris Wong <morriswch@gmail.com>
2024-09-02 11:03:03 +0100

    Enhancement and new features

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-import requests
 import json
+import requests
+import validators
+from urllib.parse import urlparse
 
 import streamlit as st
 import streamlit.components.v1 as components
 
+import groq
 from groq import Groq
 from dotenv import load_dotenv
 load_dotenv()
@@ -17,7 +20,6 @@ Traceloop.init(
     api_key=st.secrets['TRACELOOP_API_KEY']
 )
 
-
 st.set_page_config(
     page_title="Career Duck",
     page_icon="🦆",
@@ -25,34 +27,17 @@ st.set_page_config(
     initial_sidebar_state="collapsed",
 )
 
-# Set the system prompt
-system_prompt = {
-    "role": "system",
-    "content":
-    "You are a helpful assistant. You reply with very short answers."
-}
-
-# endregion
-
-# region PROMPT SETUP
-
-default_prompt = """
-You are an AI assistant that helps users write concise\
- reports on sources provided according to a user query.\
- You will provide reasoning for your summaries and deductions by\
- describing your thought process. You will highlight any conflicting\
- information between or within sources. Greet the user by asking\
- what they'd like to investigate.
-"""
-
-# system_prompt = st.sidebar.text_area("System Prompt", default_prompt, height=200)
-seed_message = {"role": "system", "content": default_prompt}
-# endregion
-
 def get_plain_text(url):
     """
         Get plain text from a url object.
     """
+    if not validators.url(url):
+        raise ValueError("Invalid URL")
+
+    parsed_url = urlparse(url)
+    if parsed_url.netloc == 'www.linkedin.com':
+        raise ValueError("URL is a LinkedIn URL")
+
     plain_text_url = 'https://r.jina.ai/{url}'
     r = requests.get(plain_text_url.format(url=url))
     return str(r.content)
@@ -63,23 +48,34 @@ def generate(prompt, deployment_name, llm='groq'):
         LLM API call
     '''
     if llm == 'groq':
-        client = Groq(api_key=st.secrets['GROQ_API_KEY'])
-        
-    completion = client.chat.completions.create(
-        model=deployment_name,
-        messages=[
-            {
-                "role": "user",
-                "content": prompt,
-            },
-        ],
-        max_tokens=3000
-    )
-    completion_json = json.loads(completion.to_json())
-    response = completion_json['choices'][0]['message']['content']
-    total_tokens = completion.usage.total_tokens
-    prompt_tokens = completion.usage.prompt_tokens
-    completion_tokens = completion.usage.completion_tokens
+        client = Groq(api_key=st.secrets['GROQ_API_KEYY'])
+    
+    try:
+        completion = client.chat.completions.create(
+            model=deployment_name,
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                },
+            ],
+            max_tokens=3000
+        )
+        completion_json = json.loads(completion.to_json())
+        response = completion_json['choices'][0]['message']['content']
+        total_tokens = completion.usage.total_tokens
+        prompt_tokens = completion.usage.prompt_tokens
+        completion_tokens = completion.usage.completion_tokens
+    except groq.APIConnectionError as e:
+        print("The server could not be reached")
+        print(e.__cause__)  # an underlying Exception, likely raised within httpx.
+    except groq.RateLimitError as e:
+        print("A 429 status code was received; we should back off a bit.")
+    except groq.APIStatusError as e:
+        print("Another non-200-range status code was received")
+        print(e.status_code)
+        print(e.response)
+
     return response, total_tokens, prompt_tokens, completion_tokens
 
 st.title("Career Duck 🦆")
@@ -88,34 +84,57 @@ form = st.form(key='my-form')
 url = form.text_input('Enter the link of the job description')
 submit = form.form_submit_button('Get bullet points')
 
-if submit:        
+if submit:
     tab1, tab2, tab3 = st.tabs(["Bullet Points", "Donate", "Feedback"])
     with tab1:
         if url:
             with st.spinner('Getting bullet points'):
-                url_str = get_plain_text(url)
-                prompt = f"""
-                    {url_str}
-                    
-                    Given this Job Description above, come up with some strong bullet points to show in a resume.
-                    Here are some examples of what make a bullet point great:
-                    OK: "Member of Leadership for Tomorrow Society"
-                    Better: "Selected as one of 275 for this 12-month professional development program for high-achieving diverse talent."
-                    Best: "Selected as one of 275 participants nationwide for this 12-month professional development program for high-achieving diverse talent based on leadership potential and academic success."
-                """
-                response, total_tokens, prompt_tokens, completion_tokens = generate(prompt, 'llama3-70b-8192')
-                tab1.subheader("Resume Bullet points suggestions")
-                st.markdown(response)
+                try:
+                    url_str = get_plain_text(url)
+                    try:
+                        prompt = f"""
+                            {url_str}
+                            
+                            Given this Job Description above, come up with some strong bullet points to show in a resume. Use the XYZ method from Google to write these bullet points
+
+                            Here are some examples of what make a bullet point great:
+                            
+                            Marketing manager 
+                            Increased page views (X) by 23% (Y) in six months by implementing social media distribution strategies (Z). 
+                            Reduced ad spend (X) by 30% (Y) by improving customer targeting (Z).
+                            
+                            Sales specialist
+                            Increased conversions (X) by 28% (Y) after training five new team members (Z). 
+                            Launched a new product (X) that led to a 15% profit increase in Q1 (Y) by engaging newsletter subscribers (Z). 
+                            
+                            Customer service 
+                            Reduced errors (X) by 40% (Y) after creating a new Standard Operating Procedure (SOP) document (Z). 
+                            Increased customer satisfaction (X) by 18% (Y) by implementing survey feedback (Z).                
+                            """
+                        response, total_tokens, prompt_tokens, completion_tokens = generate(prompt, 'llama3-70b-8192')
+
+                        tab1.subheader("Resume Bullet points suggestions")
+                        st.markdown(response)
+                    except Exception as e:
+                        st.warning("Apologies but too many people is using it now! Please try again later.")
+                except ValueError as e:
+                    if str(e) == "URL is a LinkedIn URL":
+                        st.warning("LinkedIn is strict on crwaling! Use the actual JD link on the career website dinstead.")
+                    else:
+                        st.error("Invalid URL")
+
     with tab2:
-        components.iframe(
-            src="https://ko-fi.com/morriswong/?hidefeed=true&widget=true&embed=true&preview=true",
-            width=None,
-            height=800,
-            scrolling=False,
-        )
-        # <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="morriswch" data-description="Support me on Buy me a coffee!" data-message="Let's go and land that job!" data-color="#BD5FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
+        with st.spinner('Thanks! Loading the donation page'):
+            components.iframe(
+                src="https://buymeacoffee.com/morriswch",
+                width=None,
+                height=800,
+                scrolling=False,
+            )
     with tab3:
-        feedback_form = st.form(key='feebback')
+        #TODO: Connect to Neon/ Google Sheet
+        feedback_form = st.form(key='feedback')
         feedback_text = feedback_form.text_input('Let me know what you think!')
         feedback_submit = feedback_form.form_submit_button('Send Feedback')
-        #TODO: Connect to Neon/ Google Sheet
+        if feedback_submit:
+            st.markdown('Thanks for the feedback! Hope you land your job soon!')
